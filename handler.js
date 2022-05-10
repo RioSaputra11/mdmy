2678d857c22217a418573e480f5faf94b84d6882
Rio saputra <102573606+RioSaputra11@users.noreply.github.com>
2022-05-10 15:52:25 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -94,7 +94,7 @@ module.exports = {
                     if (!isNumber(user.level)) user.level = 0
                     if (!isNumber(user.exp)) user.exp = 0
                     if (!isNumber(user.title)) user.title = ''
-                    if (!isNumber(user.limit)) user.limit = 20
+                    if (!isNumber(user.limit)) user.limit = 10
                     if (!isNumber(user.lastclaim)) user.lastclaim = 0
                     if (!isNumber(user.lastjoin)) user.lastjoin = 0
                     if (!isNumber(user.money)) user.money = 0
@@ -186,7 +186,7 @@ module.exports = {
                     level: 0,
                     title: '',
                     exp: 0,
-                    limit: 20,
+                    limit: 10,
                     lastclaim: 0,
                     lastjoin: 0,
                     money: 0,
